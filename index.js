7e853a531d66d28a40b9ad998ec8c460804bc1ba
Faisal Shakeel <97944103+FaisalShakeel@users.noreply.github.com>
2024-02-02 19:01:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT||4001
 app.get("/",(req,res)=>{
     res.json({title:"Next JS Server Side Rendering",description:"In this article,we will be learning about SSR In Next JS"})
 })
+app.get("/getprofile",(req,res)=>{
+    res.json({name:"Faisal Shakeel",bio:"Full Stack Developer",skills:["JS","TS","Next JS","React Js","Node Js"]})
+})
 app.listen(PORT,()=>{
     console.log("Listening ON PORT:",PORT)
 
